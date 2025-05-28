e4cd9363b054b72bcef59545108052672f65a186
Meliana1020 <melianaa1020@gmail.com>
2025-05-28 09:30:27 +0700

    update: test articlelist

--- src/test/ArticleList.test.tsx
+++ src/test/ArticleList.test.tsx
@@ -1,3 +1,29 @@
+jest.mock('antd', () => {
+  const actualAntd = jest.requireActual('antd');
+  const List = ({ dataSource, renderItem }: any) => (
+    <div data-testid="antd-list">
+      {dataSource && renderItem
+        ? dataSource.map((item: any, idx: number) => (
+            <div key={item.title || idx}>{renderItem(item, idx)}</div>
+          ))
+        : null}
+    </div>
+  );
+  List.Item = ({ children }: any) => <div data-testid="antd-list-item">{children}</div>;
+  const Card = ({ children, title, cover }: any) => (
+    <div data-testid="antd-card">
+      {cover}
+      <div>{title}</div>
+      {children}
+    </div>
+  );
+  return {
+    ...actualAntd,
+    List,
+    Card,
+  };
+});
+
 import { render, screen } from "@testing-library/react";
 import ArticleList from "../components/ArticleList";
 import { MemoryRouter } from "react-router-dom";
